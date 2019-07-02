964229cada2b8e7cbac8cdb3e196e0ff18952ef5
Thao Phuong Lam <lphuongt930906@gmail.com>
2019-07-02 11:11:09 +0200

    initial commit

---
+++ src/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    int data;
+    Node nextNode;
+    Node prevNode;
+
+    Node(int data) {
+        this.data = data;
+        nextNode = null;
+        prevNode = null;
+    }
+}
